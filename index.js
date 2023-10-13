8ff02e22fe30ec24764d1d3c8dcfa3ac9ec56ea5
Picuu <ltpicuuu@gmail.com>
2023-10-13 21:29:16 +0200

    feat: add permanent tasks for each month

--- index.js
+++ index.js
@@ -1,57 +1,63 @@
+const monthsNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+const tasks = ["Limpiar el baño grande", "Limpiar el baño pequeño", "Pasar la aspiradora", "Fregar el suelo"]
+
 // GET THE CURRENT MONTH
-const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 const currentMonthNumber = new Date().getMonth()
-const currentMonth = months[currentMonthNumber]
-const mesActual = document.getElementById("mes")
-mesActual.innerText = currentMonth
+const currentMonthName = monthsNames[currentMonthNumber]
 
-const monthsButtonText = document.getElementById("monthsButtonText")
-monthsButtonText.innerText = currentMonth
+const currentMonthElement = document.getElementById("mes")
+const currentMonthButtonText = document.getElementById("monthsButtonText")
 
-
-// SHOW MONTHS MENU
-const monthsMenu = document.getElementById("monthsMenu")
-const showMonthsMenu = (id) => {
-    monthsMenu.classList.toggle("displayed")
-    // const monthsListHeight = monthsMenu.offsetHeight
-    // monthsMenu.style.transform = `translateY(${monthsListHeight + 4}px)`
+const updateMonthTextElements = (monthName) => {
+    currentMonthElement.innerText = monthName
+    currentMonthButtonText.innerText = monthName
 }
+updateMonthTextElements(currentMonthName)
 
 
 // APPEND ALL THE MONTHS TO monthsList
 const monthsList = document.getElementById("monthsList")
+
 const appendMonths = () => {
-    for (const month of months) {
+    for (const month of monthsNames) {
         const li = document.createElement("li")
         li.classList.add("months-list__item")
         li.innerText = month
+        li.value = month.toLowerCase()
         monthsList.appendChild(li)
     }
 }
 appendMonths()
 
 
-// REPLACE monthsButtonText WITH THE CLICKED MONTH OF THE MENU
-const monthsListChilds = monthsList.childNodes
-for (const monthsListItem of monthsListChilds) {
-    monthsListItem.addEventListener("click", (element) => {
-        const clickedMonth = monthsListItem.innerText
-        monthsButtonText.innerText = clickedMonth
-        mesActual.innerText = clickedMonth
-        showMonthsMenu()
-        newTasks()
-    })
+// SHOW MONTHS MENU
+const showMonthsMenu = () => {
+    const monthsMenu = document.getElementById("monthsMenu")
+    monthsMenu.classList.toggle("displayed")
+}
+
+
+// TOGGLE ACTIVE CLASS WHEN A CHECKBOX IS CLICKED
+const checkboxClicked = (listRow) => {
+    const taskListRow = document.getElementById(listRow)
+    taskListRow.classList.toggle("active")
+}
+
+
+// CLEAR THE TASKS
+const taskElements = document.getElementsByClassName("task")
+
+const clearTasks = () => {
+    for (const element of taskElements) {
+        element.innerText = ""
+    }
 }
 
 
 // PUT RANDOM TASKS TO THE HOUSE GUESTS
-const tasksColumns = document.getElementsByClassName("task")
-
 const newTasks = () => {
-    const tasks = ["Limpiar el baño grande", "Limpiar el baño pequeño", "Pasar la aspiradora", "Fregar el suelo"]
-    
     const alreadyGivenTask = []
-    for (const element of tasksColumns) {
+    for (const element of taskElements) {
         let task
         do {
             task = Math.floor(Math.random() * tasks.length)
@@ -64,16 +70,45 @@ const newTasks = () => {
 newTasks()
 
 
-// CLEAR THE TASKS
-const clearTasks = () => {
-    for (const element of tasksColumns) {
-        element.innerText = ""
+const getXmlDocument = async () => {
+    const res = await fetch("./tasks.xml")
+    const xmlString = await res.text()
+    const xmlDocument = new DOMParser().parseFromString(xmlString, "text/xml")
+    return xmlDocument
+}
+
+const getTasksNumbers = (xmlDocument, selectedMonth) => {
+    const month = xmlDocument.getElementById(selectedMonth)
+    const monthTasks = month.children
+    let taskNumbers = []
+    for (const monthTask of monthTasks) {
+        const taskNumber = monthTask.textContent
+        taskNumbers.push(taskNumber)
+    }
+    return taskNumbers
+}
+
+const updateTasks = async (selectedMonth) => {
+    selectedMonth = selectedMonth.toLowerCase()
+    const xmlDocument = await getXmlDocument()
+    const taskNumbers = getTasksNumbers(xmlDocument, selectedMonth)
+    const userTasks = document.getElementsByClassName("task")
+    for (let i=0; i<4; i++) {
+        userTasks[i].innerText = tasks[taskNumbers[i]]
     }
 }
 
+// REPLACE monthsButtonText WITH THE CLICKED MONTH OF THE MENU
+const monthsListChilds = monthsList.childNodes
 
-// TOGGLE ACTIVE CLASS WHEN A CHECKBOX IS CLICKED
-const checkboxClicked = (person) => {
-    const taskListRow = document.getElementById(person)
-    taskListRow.classList.toggle("active")
+for (const monthsListItem of monthsListChilds) {
+    monthsListItem.addEventListener("click", async (element) => {
+        let clickedMonth = monthsListItem.innerText
+
+        updateMonthTextElements(clickedMonth)
+        showMonthsMenu()
+        
+        updateTasks(clickedMonth)
+    })
 }
+updateTasks(currentMonthName)
